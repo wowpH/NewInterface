bdcb12446ae9bc15c32c330da023a3b0046aac31
GitHub Action <action@github.com>
2022-08-30 04:21:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/wowph/newinterface/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wowph/newinterface/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wowph.newinterface.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wowph.newinterface.MyBundle
 
 class MyApplicationService {
 
